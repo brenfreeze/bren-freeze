e6f17a60441fb1493a523ad2e39819224fe3c818
brenfreeze <bren.aviador@whitecloak.com>
2019-12-03 15:32:32 +0800

    Added Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga'
 
 import App from './App'
 
@@ -7,6 +8,18 @@ import './assets/css/styles.css'
 
 import 'slick-carousel/slick/slick.css' 
 import 'slick-carousel/slick/slick-theme.css'
+
+
+const initializeGA = trackingCode =>
+  ReactGA.initialize(trackingCode, {
+    debug: process.env.NODE_ENV === 'development',
+    titleCase: false,
+  })
+
+const setGAPageView = page => ReactGA.pageview(page)
+
+initializeGA(process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID)
+setGAPageView(window.location.pathname + window.location.search)
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
